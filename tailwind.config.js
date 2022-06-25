0e3b240f2c9789ef334c67e846720aaf4276bbf9
fazufi <fazufi10@gmail.com>
2022-06-25 21:52:16 +0700

    jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
+  mode: 'jit',
+  purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
